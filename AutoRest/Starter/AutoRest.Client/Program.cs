7ebd863477401d7bbf20782fbcb0ff50b6b73a77
Luismi1601 <Luismi16@gmail.com>
2020-12-22 15:47:29 +0100

    commit

---
+++ AutoRest/Starter/AutoRest.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Autorest.Sdk;
+using Autorest.Sdk.Models;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Autorest.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyAPI client = new MyAPI(new Uri("http://localhost:5000"));
+            IList<Destination> destinationList = client.ApiDestinationsGet();
+
+            WriteLine("\nAll Destination");
+            foreach (Destination destination in destinationList)
+            {
+                WriteLine($"{destination.CityName} - {destination.Airport}");
+            }
+            // ReadKey used that the console will not close when the code end to run.
+            ReadKey();
+        }
+    }
+}
---
+++ HttpClientApplication/Starter/HttpClientApplication.Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using HttpClientApplication.Host.Models;
+using System.Runtime.Serialization.Json;
+using static System.Console;
+
+namespace HttpClientApplication.Client
+{
+    class Program
+    {
+        static  async Task Main(string[] args)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, clientCertificateOption,ContextMarshalException,sslPolicyErrors) => {return true;};
+            
+            using (var client = new HttpClient(clientHandler)) //= new HttpClientHandler(); 
+            //clientHandler.ServerCertificateCustomValidationCallback))
+            {
+                HttpResponseMessage message = await client.GetAsync("https://localhost:5001/api/destinations");
+                WriteLine("Respone data as string");
+                string resultAsString = await message.Content.ReadAsStringAsync();
+                WriteLine(resultAsString);
+                
+                List<Destination> destinationsResult = await message.Content.ReadAsAsync<List<Destination>>();
+                WriteLine("\nAll Destination");
+                foreach (Destination destination in destinationsResult)
+                {
+                    WriteLine($"{destination.CityName} - {destination.Airport}");
+                }
+                // ReadKey used that the console will not close when the code end to run.
+                ReadKey();
+            }
+        }
+    }
+}
